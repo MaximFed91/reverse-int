d14606c8cb353ef3f4af8256c5637b15367b5519
Maksim <honestliar91@gmail.com>
2021-07-14 16:20:33 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) { 
+        return +String(n).split('').slice(1).reverse().join('');
+    } else {
+        return +String(n).split('').reverse().join('');
+    }
+ 
 }
